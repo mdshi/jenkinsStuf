2d6a23c7a73d8a3846689553ea09a81946d66205
mdshi <mikeshifera@gmail.com>
2018-07-01 03:12:59 -0500

    adding groovy file to wipe clean jobs

---
+++ WipeClean.groovy
@@ -0,0 +1,15 @@
+import jenkins.model.Jenkins
+
+String [] Jobs_To_Be_Deleted = [
+    "Hello"
+]
+
+void cleanUPtheNodes(Node node, item_to_clean)
+{
+    println("${node.name}:")
+
+      workspacePath = node.getWorkspaceFor(item_to_wipe)
+  if(!workspacePath){
+    println("    Failed to find workspace path on ${node.name}.")
+    return
+}
